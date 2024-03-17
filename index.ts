0aef6994207a47707b75f9865b6283c3684ce06e
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-03-17 20:30:12 +0500

    format_document

--- index.ts
+++ index.ts
@@ -7,7 +7,13 @@ const answer = await inquirer.prompt([
     message: "select one of the operator from operation",
     name: "operator",
     type: "list",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division", "Percentage"],
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Multiplication",
+      "Division",
+      "Percentage",
+    ],
   },
 ]);
 // console.log(`firstvalue: ${answer.firstNumber}, secondvalue: ${answer.secondNumber},operator:${answer.operator}`)
@@ -21,6 +27,6 @@ if (answer.operator === "Addition") {
   console.log(answer.firstNumber * answer.secondNumber);
 } else if (answer.operator === "Division") {
   console.log(answer.firstNumber / answer.secondNumber);
-}else if (answer.operator === "Percentage"){
-    console.log(answer.firstNumber / answer.secondNumber * 100);
+} else if (answer.operator === "Percentage") {
+  console.log((answer.firstNumber / answer.secondNumber) * 100);
 } else console.log("Please enter valid operator");
